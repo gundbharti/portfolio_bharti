b6674632200e61da3da03069373e3485624e7a58
gundbharti <140239748+gundbharti@users.noreply.github.com>
2025-01-26 22:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* ====================== typing animation ============================= */
+
+var typed = new Typed(".typing" ,{
+    strings:["","Web Developer", "web Designer", "React Developer" , "Frontend Developer"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
